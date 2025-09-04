8dc905ff65be87e78196facd2d534f89a6d2fc27
diddyduden <diddysmain@gmail.com>
2025-09-04 13:40:46 +0200

    d
    d

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-#include <raylib.h>
+#include "raylib.h"
 
-using namespace std;
+int main() {
 
-void main() {
-	cout << "Hello!";
+    //game definitions
+    const int width = 700;
+    const int height = 700;
+
+    //colors
+    Color snakeGrass = {255, 214, 222, 255 };
+
+    InitWindow(width, height, "Snake");
+    SetTargetFPS(60);
+
+    //game loop
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(snakeGrass);
+
+        DrawText("Snake spel det bara börja liksom!", 200, 280, 30, LIGHTGRAY);
+
+
+
+
+
+
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
 }
