8ef3d58575419e09af91dc04c5a9fba979a5084c
diddyduden <diddysmain@gmail.com>
2025-09-05 17:47:27 +0200

    changes here and there ion know
    dsds

--- src/main.cpp
+++ src/main.cpp
@@ -11,30 +11,26 @@ private:
     int snakeY = 100;
 
     bool isCollided = false;
-    Color snakeColor = RED;
+    Color snakeColor = SKYBLUE;
 
 public:
     //methods
-    int movement() {
+    void movement() {
         if (IsKeyPressed(KEY_W)) {
             snakeY -= 64;
-            return snakeY;
-            std::cout << "pressed W key";
+            std::cout << "pressed W key" << std::endl;
         }
         if (IsKeyPressed(KEY_A)) {
             snakeX -= 64;
-            return snakeX;
-            std::cout << "pressed A key";
+            std::cout << "pressed A key" << std::endl;
         }
         if (IsKeyPressed(KEY_S)) {
             snakeY += 64;
-            return snakeY;
-            std::cout << "pressed S key";
+            std::cout << "pressed S key" << std::endl;
         }
         if (IsKeyPressed(KEY_D)) {
             snakeX += 64;
-            return snakeX;
-            std::cout << "pressed D key";
+            std::cout << "pressed D key" << std::endl;
         }
     }
 
@@ -51,45 +47,60 @@ public:
     }
 
     //get 
-    int getSnakeX() {
-        return snakeX;
-    }
-    int getSnakeY() {
-        return snakeY;
-    }
-    bool getIsCollided() {
-        return isCollided;
-    }
-    int getSnakeWidth() {
-        return snakeWidth;
-    }
-    int getSnakeHeight() {
-        return snakeHeight;
-    }
-    Color getColor() {
-        return snakeColor;
-    }
+    int getSnakeX() { return snakeX; }
+    int getSnakeY() { return snakeY; }
+
+    bool getIsCollided() { return isCollided; }
+
+    int getSnakeWidth() { return snakeWidth; }
+    int getSnakeHeight() { return snakeHeight; }
+
+    Color getColor() { return snakeColor; }
 
     //sets
-    void setSnakeX(int xPos) {
-        snakeX = xPos;
-    }
-    void setSnakeY(int yPos) {
-        snakeY = yPos;
-    }
-    void setIsCollided(bool collided) {
-        isCollided = collided;
-    }   
+    void setSnakeX(int xPos) { snakeX = xPos; }
+    void setSnakeY(int yPos) { snakeY = yPos; }
+
+    void setIsCollided(bool collided) { isCollided = collided; }   
 };
 
 class SnakeCube : SnakeHead {
+private:
+
+
+public:
+
 
 };
 
 class Apple {
-    //check for snake pos
-    //apple can spawn anywhere but snake location
+private:
+    const int appleWidth = 60;
+    const int appleLength = 60;
 
+    //random spawn
+    int appleX = GetRandomValue(700, 700);
+    int appleY = GetRandomValue(700, 700);
+
+public:
+    //methods
+    void appleSpawn() {
+        if ()
+    }
+
+
+
+
+    //get
+    int getAppleWidth() { return appleWidth; }
+    int getAppleLength() { return appleLength; }
+
+    int getAppleX() { return appleX; }
+    int getAppleY() { return appleY; }
+
+    //set
+    void setAppleX(int xPos) { appleX = xPos; };
+    void setAppleY(int yPos) { appleY = yPos; };
 };
 
 int main() {
@@ -102,32 +113,39 @@ int main() {
 
     //colors
     Color snakeGrass = {161, 255, 104, 255 };
-
+        
+    //initialize things
+    SnakeHead snake;
 
     //game loop
     while (WindowShouldClose() == false) {
         BeginDrawing();
+
+        //bakgrunden
         ClearBackground(snakeGrass);
 
-        //skapa snakeHead
-        SnakeHead snake;
+        //snake egenskaper o sånt idk
+        snake.movement();
         DrawRectangle(snake.getSnakeX(), snake.getSnakeY(), snake.getSnakeHeight(), snake.getSnakeHeight(), snake.getColor());
-        snake.update();
+
+        //apple
+        DrawRectangle(120, 150, 50, 50, RED);
+
+
 
         EndDrawing();
     }
     CloseWindow();
 }
 
-
-// pink color
+/* pink color
 // Color snakeGrass = { 255, 214, 222, 255 };
 //behöver ingen constructor för alla värden kommer ju startas likadant?
 
 //theme? ändra tema (färger på orm bakgrund osv)
 
 
-        /*
+        
         //wasd keybinds
         if (IsKeyPressed(KEY_W)) {
             snakeY -= 64;
